163e0e2b01af16c473a4394bbc539fedc2bcfa17
eric1313 <eric1313@live.com>
2015-12-17 12:50:15 -0500

    Early Map Generation

--- src/map/Map.java
+++ src/map/Map.java
@@ -1,5 +1,9 @@
 package map;
 
+import java.awt.Point;
+
+import com.sun.javafx.scene.traversal.Direction;
+
 /**
  * Map of a game of Project Z.
  * 
@@ -8,5 +12,58 @@ package map;
  * @since 1.0
  * @version 1.0
  */
-public class Map {
+public class Map
+{
+
+	int height;
+	int width;
+	final int minArea = 625;
+	final int maxArea = 2500;
+	char[][] map;
+
+	public Map(int height, int width)
+	{
+		this.width = width;
+		this.height = height;
+		this.map = new char[width][height];
+
+		// Generate main road
+
+		int startingPoint = (int) (width / 4 + Math.random() * (width / 2));
+		generateVertiacalRoad(startingPoint, 0, Direction.DOWN, 23);
+
+	}
+
+	public void generateSideRoads(Point start, Point end, double xyRatio, int minArea)
+	{
+	int boxWidth=(int) Math.abs(end.getX()-start.getX());
+	int boxHeight=(int)Math.abs(end.getY()-start.getY()) > maxArea);
+		if (Math.abs(end.getY()-start.getY()) > maxArea){
+		
+	if(Math.random()>.5)
+	{
+		generateVertiacalRoad(startingPoint, 0, Direction.DOWN, 23);
+	}
+	else
+	{
+		generateVertiacalRoad(, 0, Direction.DOWN, 23);
+
+	}
+	
+		}
+	else
+	{
+	}
+
+	public void generateVertiacalRoad(int x, int y, Direction direction,
+			int width)
+	{
+
+	}
+
+	public void generateHorizontalRoad(int width)
+	{
+
+	}
+
 }
