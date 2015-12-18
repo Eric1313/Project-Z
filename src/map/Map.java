e7e07bf01fbd2bf66b6df80b7686c2021b915d57
eric1313 <eric1313@live.com>
2015-12-18 12:48:52 -0500

    Updated road generation

--- src/map/Map.java
+++ src/map/Map.java
@@ -17,12 +17,13 @@ public class Map
 
 	int height;
 	int width;
+	int minSideLength = 24;
 	final int maxArea = 2500;
-	final double lengthWidthRatio=0.5;
+	final double heightWidthRatio = 0.5;
 
 	char[][] map;
 
-	public Map(int height, int width) 
+	public Map(int height, int width)
 	{
 		this.width = width;
 		this.height = height;
@@ -31,37 +32,69 @@ public class Map
 		// Generate main road
 
 		int startingPoint = (int) (width / 4 + Math.random() * (width / 2));
-		generateVertiacalRoad(startingPoint, 0, Direction.DOWN, 23);
+		generateVertiacalRoad(startingPoint, 0, 13);
 
 	}
 
-	public void generateSideRoads(Point start, Point end, double xyRatio,
-			int minArea)
+	public void generateSideRoads(Point start, Point end)
 	{
 		int boxWidth = (int) (Math.abs(end.getX() - start.getX()));
 		int boxHeight = (int) (Math.abs(end.getY() - start.getY()));
-		int startingPoint;
+		int startingX;
+		int startingY;
 		if (boxWidth * boxHeight > maxArea)
 		{
-			if (Math.random() > .5)
+			if (boxWidth > 2 * minSideLength && Math.random() > .5)
 			{
-				
-				// generateVertiacalRoad(startingPoint, 0, Direction.DOWN, 7);
+				startingX = (int) (Math.min(start.getX(), end.getX()) + (minSideLength + (Math
+						.random() * (boxWidth - minSideLength))));
+				startingY = (int) Math.min(start.getY(), end.getY());
+				generateVertiacalRoad(startingX, startingY, 7);
+				// Recursive split new generated squares
+				Point start1 = start;
+				Point end1 = new Point(startingX - 3, startingY);
+				generateSideRoads(start1, end1);
+				Point start2 = new Point(startingX + 3, (int) start.getY());
+				Point end2 = end;
+				generateSideRoads(start2, end2);
+
+			}
+			else if (boxHeight > 2 * minSideLength)
+			{
+				startingY = (int) (Math.min(start.getY(), end.getY()) + (minSideLength + (Math
+						.random() * (boxHeight - minSideLength))));
+				startingX = (int) Math.min(start.getX(), end.getX());
+				generateVertiacalRoad(startingX, startingY, 7);
+				// Recursive split new generated squares
+				Point start1 = start;
+				Point end1 = new Point((int) end.getX(), startingY + 3);
+				generateSideRoads(start1, end1);
+				Point start2 = new Point((int) start.getX(), startingY - 3);
+				Point end2 = end;
+				generateSideRoads(start2, end2);
 			}
 			else
 			{
-				// generateHorizontalRoad(startingPoint 0, Direction.WEST, 7);
+				// generate plaza
 			}
+		}
+		else
+		{
+			// generate plaza
 		}
 	}
 
-	public void generateVertiacalRoad(int x, int y, Direction direction,
+	public void generateVertiacalRoad(int x, int y, 
 			int width)
 	{
-
+		int 
+		while(map[x][y]!=null)
+		{
+			
+		}
 	}
 
-	public void generateHorizontalRoad(int x, int y, Direction direction,
+	public void generateHorizontalRoad(int x, int y,
 			int width)
 	{
 
