be3f9313927f3910bdc745cbbaafdc1887bc29ae
onetrueallen <allenhan348@gmail.com>
2016-01-05 12:44:46 -0500

    Loaded all assets.

--- src/utilities/SpriteSheet.java
+++ src/utilities/SpriteSheet.java
@@ -4,28 +4,58 @@
  * @version June, 15th, 2015
  */
 package utilities;
+
 import java.awt.image.BufferedImage;
 
 public class SpriteSheet {
 	private BufferedImage sheet;
+	private int width;
+	private int height;
 
 	/**
 	 * Constructor for the sprite sheet
-	 * @param sheet the sprite sheet image
+	 * 
+	 * @param sheet
+	 *            the sprite sheet image
 	 */
 	public SpriteSheet(BufferedImage sheet) {
 		this.sheet = sheet;
+		width = sheet.getWidth();
+		height = sheet.getHeight();
 	}
-	
+
 	/**
 	 * Crops the image to the correct sprite
-	 * @param x the x position of the sprite
-	 * @param y the y position of the sprite
-	 * @param width the width of the sprite
-	 * @param height the heigh of the sprite
+	 * 
+	 * @param x
+	 *            the x position of the sprite
+	 * @param y
+	 *            the y position of the sprite
+	 * @param width
+	 *            the width of the sprite
+	 * @param height
+	 *            the height of the sprite
 	 * @return
 	 */
-	public BufferedImage crop (int x, int y, int width, int height){
+	public BufferedImage crop(int x, int y, int width, int height) {
 		return sheet.getSubimage(x, y, width, height);
+	}
+
+	/**
+	 * Get the width of the sprite sheet
+	 * 
+	 * @return the width of the sprite sheet
+	 */
+	public int getWidth() {
+		return width;
+	}
+
+	/**
+	 * Get the height of the sprite sheet
+	 * 
+	 * @return the width of the sprite sheet
+	 */
+	public int getHeight() {
+		return height;
 	}
 }
