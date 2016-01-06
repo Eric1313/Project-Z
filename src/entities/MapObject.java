2620dc326063f41cbdf7118698dd6d16eeac9193
eric1313 <eric1313@live.com>
2016-01-06 12:15:11 -0500

    Deleted old map classes, added mob, deleted redundent entities, added item effects

--- src/entities/MapObject.java
+++ src/entities/MapObject.java
@@ -1,12 +1,12 @@
 package entities;
 
 /**
- * Enumeration for specifying the different types of debris.
+ * Abstract MapObject class for objects inside a map in Project Z.
  * 
  * @author Patrick Liu, Eric Chee, Allen Han, Alosha Reymer
- * @see Debris
+ * @see Map
  * @since 1.0
  * @version 1.0
  */
-public enum DebrisType {
+public abstract class MapObject {
 }
