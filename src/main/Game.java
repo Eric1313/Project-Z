004b87b40d0c26a02df7c774ea8c19e311934c5e
onetrueallen <allenhan348@gmail.com>
2016-01-06 12:07:14 -0500

    Fixed Game object

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,15 +1,10 @@
-package server;
-
-import client.Client;
+package main;
 
 public class Game implements Runnable {
-
 	@Override
 	public void run() {
 	}
 
 	public static void main(String[] args) {
-		new Thread(new Server(5000)).start();
-		new Client("127.0.0.1", 5000);
 	}
 }
