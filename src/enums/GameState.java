0a2d4c834d7df605ec4cceef7414387af010639e
onetrueallen <allenhan348@gmail.com>
2016-01-06 12:48:30 -0500

    Added rendering methods to the game state. Selected the correct state to render in the Game.

--- src/enums/GameState.java
+++ src/enums/GameState.java
@@ -1,5 +1,26 @@
 package enums;
 
-public enum GameState {
-	LOBBY, INGAME
+public class GameState {
+	public enum State {
+		LOBBY, INGAME
+	}
+
+	private State gameState;
+
+	public void update() {
+		switch (gameState) {
+		case LOBBY:
+			break;
+		case INGAME:
+			break;
+		}
+	}
+
+	public State getGameState() {
+		return gameState;
+	}
+
+	public void setGameState(State gameState) {
+		this.gameState = gameState;
+	}
 }
--- src/main/Game.java
+++ src/main/Game.java
@@ -1,8 +1,8 @@
 package main;
 
 import utilities.Assets;
-import states.GameState;
-
+import enums.GameState;
+import enums.GameState.State;
 public class Game implements Runnable {
 	private Assets tiles;
 	
@@ -10,11 +10,14 @@ public class Game implements Runnable {
 	private String title;
 	private int width;
 	private int height;
+	
+	private GameState state;
 
 	public Game(String title, int width, int height) {
 		this.title = title;
 		this.width = width;
 		this.height = height;
+	
 		initialize();
 	}
 
@@ -22,7 +25,12 @@ public class Game implements Runnable {
 		// Loads the tile assets
 		tiles = new Assets("res/img/tiles.png");
 
+		// Loads the display
 		display = new Display(title, width, height);
+		
+		// Sets the state of the game
+		GameState state = new GameState();
+		state.setGameState(State.INGAME);
 	}
 
 	@Override
