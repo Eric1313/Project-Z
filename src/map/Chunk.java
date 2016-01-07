b0a2a3eb55627d326b5184161f4ac9b1b1406472
eric1313 <eric1313@live.com>
2016-01-07 17:55:55 -0500

    Added entity list to chunks

--- src/map/Chunk.java
+++ src/map/Chunk.java
@@ -1,5 +1,70 @@
 package map;
 
-public class Chunk {
+import java.util.ArrayList;
+import entities.Entity;
 
+public class Chunk {
+	 private ArrayList<Entity> solidEntities;
+	 private ArrayList<Entity> passibleEntities;
+
+	public Chunk()
+	{
+		solidEntities=new ArrayList<Entity>();
+		passibleEntities= new  ArrayList<Entity>();
+	}
+	
+	/**
+	 * Adds entity to chunk
+	 * @param entity entity to add
+	 */
+	public void add(Entity entity)
+	{
+		if (entity.isSolid())
+			solidEntities.add(entity);
+		else
+			passibleEntities.add(entity);
+	}
+	
+	/**
+	 * Removes Entities from chunks
+	 * @param entity
+	 * @return
+	 */
+	public Entity remove(Entity entity)
+	{
+		if (entity.isSolid())
+			solidEntities.remove(entity);
+		else
+			passibleEntities.remove(entity);
+		return entity;
+	}
+
+	/**
+	 * @return the solidEntities
+	 */
+	public ArrayList<Entity> getSolidEntities() {
+		return solidEntities;
+	}
+
+	/**
+	 * @param solidEntities the solidEntities to set
+	 */
+	public void setSolidEntities(ArrayList<Entity> solidEntities) {
+		this.solidEntities = solidEntities;
+	}
+
+	/**
+	 * @return the passibleEntities
+	 */
+	public ArrayList<Entity> getPassibleEntities() {
+		return passibleEntities;
+	}
+
+	/**
+	 * @param passibleEntities the passibleEntities to set
+	 */
+	public void setPassibleEntities(ArrayList<Entity> passibleEntities) {
+		this.passibleEntities = passibleEntities;
+	}
+	
 }
