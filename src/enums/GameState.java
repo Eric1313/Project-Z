8cde0ede8352a6a728bf73ebe1d1ea9a77ce36d9
pcjl <pcjl@users.noreply.github.com>
2016-01-07 12:18:39 -0500

    Added class javadoc comments for all enums classes

--- src/enums/GameState.java
+++ src/enums/GameState.java
@@ -1,5 +1,13 @@
 package enums;
 
+/**
+ * GameState object for storing and changing the game state of the game.
+ * 
+ * @author Allen Han, Alosha Reymer, Eric Chee, Patrick Liu
+ * @see main.Game
+ * @since 1.0
+ * @version 1.0
+ */
 public class GameState {
 	public enum State {
 		LOBBY, INGAME
