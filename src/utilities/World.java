5663a7c19f916ae3b752382fc516e91a827409fd
onetrueallen <onetrueallen@users.noreply.github.com>
2016-01-07 19:10:42 -0500

    Fixing rendering

--- src/utilities/World.java
+++ src/utilities/World.java
@@ -13,25 +13,24 @@ public class World {
 
 	public World(BufferedImage[][] tiles) {
 		this.tiles = tiles;
-//		try {
-//			map = new Map(1600, 1600);
-//		} catch (FileNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	tileId = map.getMap();
+		try {
+			map = new Map(1600, 1600);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	tileId = map.getMap();
 	}
 
 	public void render(Graphics g) {		
-		g.drawImage(tiles[0][1], 0, 0, null);
-//		for (int i = 0; i < 34; i++) {
-//			for (int j = 0; j < 60; j++) {
-//				int a = (tileId[j][i] & 0xFFF);
-//				g.drawImage(tiles[(a / 100) - 1][a % 100], (a % 100)
-//						* Assets.tileWidth,
-//						((a / 100) - 1) * Assets.tileHeight, null);
-//			}
-//		}
-//		System.out.println ("UPDATING");
+		for (int i = 0; i < 34; i++) {
+			for (int j = 0; j < 60; j++) {
+				int a = (tileId[j][i] & 0xFFF);
+				g.drawImage(tiles[(a / 100) - 1][a % 100], (a % 100)
+						* Assets.tileWidth,
+						((a / 100) - 1) * Assets.tileHeight, null);
+			}
+		}
+		System.out.println ("UPDATING");
 	}
 }
