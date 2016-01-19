c6a00fb35fb3ebe397f030d65e00f2696c6ff07c
onetrueallen <onetrueallen@users.noreply.github.com>
2016-01-18 23:25:48 -0500

    Movement speed fixed

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -35,7 +35,7 @@ import utilities.MouseHandler;
  * @version 1.0
  */
 public class Player extends Mob {
-	public static final int MOVEMENT_SPEED = 6;
+	public static final int MOVEMENT_SPEED = 2;
 	public static final int MAX_STAMINA = 300;
 	public static final int MIN_STAMINA = MAX_STAMINA / 10;
 	public static final int SPRINT_COST = Player.MAX_STAMINA / 300;
